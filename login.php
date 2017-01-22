f56846ff6cb4e2256a2b93bff97a166d56032abf
mayur19 <mayurdusane1@gmail.com>
2017-01-22 12:16:52 +0530

    Login and signup is stable with profile integration

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-    $sql = "SELECT * FROM dms_users WHERE username = '$myusername' AND password = '$mypassword' AND status=1";
+    $sql = "SELECT * FROM dms_users WHERE username = '$myusername' AND password = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     //$active = $row['active'];
@@ -24,40 +24,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['username'] = $myusername;
         $_SESSION['fullname'] = $row['fullname'];
         $_SESSION['role'] = $row['role'];
-        echo $row['role'];
+
+        if($row['status'] == 0) {
+            echo "deactive";
+        }else {
+            echo $row['role'];
+        }
        // echo $row['user_id'];
     }
-    /*
-    else {
-        echo "<html>
-                <head>
-                <title>Error</title>
-                <link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
-                <link href='css/bootstrap.min.css' rel='stylesheet'>
-                <link rel= 'stylesheet' href='css/style.css'>
-                <script src='https://code.jquery.com/jquery-1.12.4.js'></script>
-                <script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
-                <script>
-                $( function() {
-                    $( '#dialog' ).dialog();
-                } );
-                </script>
-                </head>
-                <body>
 
-                <div id='dialog' title='Login Failed'>
-                <div class='row'>
-                <p>Invalid Username or password</p>
-                </div>
-                <div class='row'>
-                <div class='col-xs-2 col-xs-offset-4'>
-                <a href='index.html'type='button' class='btn btn-primary'>Re-Login</a>
-                </div>
-                </div>
-                </div>
-                </body>
-                </html>";
-    }
-    */
 }
 ?>
